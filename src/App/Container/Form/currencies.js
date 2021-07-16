e14e98f79061ff7e7b6053317f3107d83fc1f708
Krystian Kaźmierczak <s155774@gmail.com>
2021-07-16 22:03:46 +0200

    code cleaning

--- src/App/Container/Form/currencies.js
+++ src/App/Container/Form/currencies.js
@@ -1,29 +1,27 @@
-const currencies = [
-    {
-        shortcut: "EUR",
-        name: "Euro",
-        rate: 4.5467,
-    },
-    {
-        shortcut: "USD",
-        name: "Dolar amerykański",
-        rate: 3.8427,
-    },
-    {
-        shortcut: "GBP",
-        name: "Funt brytyjski",
-        rate: 5.2942,
-    },
-    {
-        shortcut: "CHF",
-        name: "Frank szwajcarski",
-        rate: 4.1965,
-    },
-    {
-        shortcut: "TRY",
-        name: "Lira turecka",
-        rate: 0.4423,
-    },
+export const currencies = [
+  {
+    shortcut: "EUR",
+    name: "Euro",
+    rate: 4.5467,
+  },
+  {
+    shortcut: "USD",
+    name: "Dolar amerykański",
+    rate: 3.8427,
+  },
+  {
+    shortcut: "GBP",
+    name: "Funt brytyjski",
+    rate: 5.2942,
+  },
+  {
+    shortcut: "CHF",
+    name: "Frank szwajcarski",
+    rate: 4.1965,
+  },
+  {
+    shortcut: "TRY",
+    name: "Lira turecka",
+    rate: 0.4423,
+  },
 ];
-
-export {currencies};
